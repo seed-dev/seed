c87ec69615f7e8ff158e8b55f0ad3dc6062ab1ff
EugeneHeen <eugeneheen_chen@126.com>
2017-09-01 17:53:38 +0800

    定义HTTP状态码枚举

---
+++ seedm-entities/src/main/java/com/github/seedm/entities/enumeration/StatusCodeEnum.java
@@ -0,0 +1,21 @@
+package com.github.seedm.entities.enumeration;
+
+/**
+ * Http状态码枚举
+ * @author Eugene
+ */
+public enum StatusCodeEnum {
+
+    OK(200), BADREQUEST(400), NOTFOUND(404),
+    INTERNALSERVERERROR(500), BADGATEWAY(502), SERVICEUNAVAILABLE(503), GATEWAYTIMEOUT(504), HTTPVERSIONNOTSUPPORTED(505);
+
+    private int statusCode;
+
+    StatusCodeEnum(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
