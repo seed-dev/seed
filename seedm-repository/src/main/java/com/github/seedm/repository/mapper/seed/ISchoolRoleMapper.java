92729624521bcf53b5430eb39208f67bc8e9ea5b
EugeneHeen <eugeneheen_chen@126.com>
2017-09-04 16:01:11 +0800

    实现学校角色关系维护数据操作

---
+++ seedm-repository/src/main/java/com/github/seedm/repository/mapper/seed/ISchoolRoleMapper.java
@@ -0,0 +1,16 @@
+package com.github.seedm.repository.mapper.seed;
+
+import com.github.seedm.repository.vo.seed.SchoolRoleVo;
+
+/**
+ * 学校角色关系表映射操作
+ */
+public interface ISchoolRoleMapper {
+
+    /**
+     * 新增学校角色关系绑定
+     * @param schoolRoleVo 学校角色关系绑定信息
+     * @return 成功绑定数据条数
+     */
+    public int insert(SchoolRoleVo schoolRoleVo);
+}
---
+++ seedm-repository/src/test/java/com/github/seedm/test/repository/SchoolRoleMapperTest.java
@@ -0,0 +1,43 @@
+package com.github.seedm.test.repository;
+
+import com.github.seedm.repository.mapper.seed.IAccountSchoolMapper;
+import com.github.seedm.repository.mapper.seed.ISchoolRoleMapper;
+import com.github.seedm.repository.vo.seed.AccountSchoolVo;
+import com.github.seedm.repository.vo.seed.SchoolRoleVo;
+import com.github.toolkit.core.CodecKit;
+import com.github.toolkit.core.StringKit;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-core.xml"})
+@Transactional
+public class SchoolRoleMapperTest {
+
+    @Autowired
+    private ISchoolRoleMapper schoolRoleMapper;
+
+    private static CodecKit codecKit;
+
+    private static StringKit stringKit;
+
+    @BeforeClass
+    public static void beforClass() {
+        codecKit = new CodecKit();
+        stringKit = new StringKit();
+    }
+
+    @Test
+    public void testInsert() {
+//        SchoolRoleVo schoolRoleVo = new SchoolRoleVo(stringKit.uuid(true), "84961878dadc41fc8c3553f0de76eaa8", "076aeb9d097b4a6187f7497028dbaf83");
+        SchoolRoleVo schoolRoleVo = new SchoolRoleVo(stringKit.uuid(true), "84961878dadc41fc8c3553f0de76eaa8", "357a91e6f92e40a6bb0039f398788d85");
+        int count = this.schoolRoleMapper.insert(schoolRoleVo);
+        Assert.assertEquals(1, count);
+    }
+}
