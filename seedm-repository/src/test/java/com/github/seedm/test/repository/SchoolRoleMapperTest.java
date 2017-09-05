411b1c213cf89de8d4a7cf5ce39573da21b79578
EugeneHeen <eugeneheen_chen@126.com>
2017-09-05 20:14:29 +0800

    将账号学校关系映射实现变为学生学校关系映射

--- seedm-repository/src/test/java/com/github/seedm/test/repository/SchoolRoleMapperTest.java
+++ seedm-repository/src/test/java/com/github/seedm/test/repository/SchoolRoleMapperTest.java
@@ -1,8 +1,6 @@
 package com.github.seedm.test.repository;
 
-import com.github.seedm.repository.mapper.seed.IAccountSchoolMapper;
 import com.github.seedm.repository.mapper.seed.ISchoolRoleMapper;
-import com.github.seedm.repository.vo.seed.AccountSchoolVo;
 import com.github.seedm.repository.vo.seed.SchoolRoleVo;
 import com.github.toolkit.core.CodecKit;
 import com.github.toolkit.core.StringKit;
