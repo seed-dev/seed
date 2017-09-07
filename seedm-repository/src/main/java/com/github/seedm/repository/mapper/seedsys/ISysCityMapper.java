107307cf8c793a63c395831ea523c179bc4245c1
EugeneHeen <eugeneheen_chen@126.com>
2017-09-07 13:07:29 +0800

    修改注释

--- seedm-repository/src/main/java/com/github/seedm/repository/mapper/seedsys/ISysCityMapper.java
+++ seedm-repository/src/main/java/com/github/seedm/repository/mapper/seedsys/ISysCityMapper.java
@@ -5,15 +5,15 @@ import com.github.seedm.repository.vo.seedsys.SysCityVO;
 import java.util.List;
 
 /**
- * 系统数据库城市表映射定义
+ * 系统数据库市级信息表映射定义
  * @author Eugene
  */
 public interface ISysCityMapper {
 
     /**
-     * 根据省份编码查询系统数据库全部城市信息
+     * 根据省份编码查询系统数据库全部市级信息
      * @param provinceCode 省份编码
-     * @return 城市信息列表
+     * @return 市级信息列表
      */
     public List<SysCityVO> selectByProvince(String provinceCode);
 }
