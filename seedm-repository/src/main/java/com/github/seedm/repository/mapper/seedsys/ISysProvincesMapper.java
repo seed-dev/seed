c5a5bd58ac1f1704e1c8ad436c470669920c233a
EugeneHeen <eugeneheen_chen@126.com>
2017-09-07 13:01:45 +0800

    实现查询全部身份数据接口

--- seedm-repository/src/main/java/com/github/seedm/repository/mapper/seedsys/ISysProvincesMapper.java
+++ seedm-repository/src/main/java/com/github/seedm/repository/mapper/seedsys/ISysProvincesMapper.java
@@ -1,8 +1,18 @@
 package com.github.seedm.repository.mapper.seedsys;
 
+import com.github.seedm.repository.vo.seedsys.SysProvincesVO;
+
+import java.util.List;
+
 /**
  * 系统数据库省份表映射定义
  * @author Eugene
  */
 public interface ISysProvincesMapper {
+
+    /**
+     * 查询系统数据库全部省份信息
+     * @return 省份信息列表
+     */
+    public List<SysProvincesVO> selectAll();
 }
---
+++ seedm-repository/src/test/java/com/github/seedm/test/repository/SysProvincesMapperTest.java
@@ -0,0 +1,29 @@
+package com.github.seedm.test.repository;
+
+
+import com.github.seedm.repository.mapper.seedsys.ISysProvincesMapper;
+import com.github.seedm.repository.vo.seedsys.SysProvincesVO;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-core.xml"})
+@Transactional
+public class SysProvincesMapperTest {
+
+    @Autowired
+    private ISysProvincesMapper sysProvincesMapper;
+
+    @Test
+    public void testSelectAll() {
+        List<SysProvincesVO> sysProvinces = this.sysProvincesMapper.selectAll();
+        Assert.assertEquals(34, sysProvinces.size());
+    }
+}
