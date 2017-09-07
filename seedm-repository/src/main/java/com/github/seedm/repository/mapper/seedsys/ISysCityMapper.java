f2c0e181a852049362dd3406c0d0328658c92708
EugeneHeen <eugeneheen_chen@126.com>
2017-09-07 13:02:30 +0800

    实现查询全部所属省份编码的市级

--- seedm-repository/src/main/java/com/github/seedm/repository/mapper/seedsys/ISysCityMapper.java
+++ seedm-repository/src/main/java/com/github/seedm/repository/mapper/seedsys/ISysCityMapper.java
@@ -1,8 +1,19 @@
 package com.github.seedm.repository.mapper.seedsys;
 
+import com.github.seedm.repository.vo.seedsys.SysCityVO;
+
+import java.util.List;
+
 /**
  * 系统数据库城市表映射定义
  * @author Eugene
  */
 public interface ISysCityMapper {
+
+    /**
+     * 根据省份编码查询系统数据库全部城市信息
+     * @param provinceCode 省份编码
+     * @return 城市信息列表
+     */
+    public List<SysCityVO> selectByProvince(String provinceCode);
 }
---
+++ seedm-repository/src/test/java/com/github/seedm/test/repository/SysCityMapperTest.java
@@ -0,0 +1,34 @@
+package com.github.seedm.test.repository;
+
+
+import com.github.seedm.repository.mapper.seedsys.ISysCityMapper;
+import com.github.seedm.repository.mapper.seedsys.ISysProvincesMapper;
+import com.github.seedm.repository.vo.seedsys.SysCityVO;
+import com.github.seedm.repository.vo.seedsys.SysProvincesVO;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-core.xml"})
+@Transactional
+public class SysCityMapperTest {
+
+    @Autowired
+    private ISysCityMapper sysCityMapper;
+
+    @Test
+    public void testSelectAll() {
+        //查询四川省的所有市级
+        List<SysCityVO> sysCities = this.sysCityMapper.selectByProvince("510000");
+
+        //总共21个市级
+        Assert.assertEquals(21, sysCities.size());
+    }
+}
