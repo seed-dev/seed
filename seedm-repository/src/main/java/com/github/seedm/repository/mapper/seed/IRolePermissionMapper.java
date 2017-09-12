65ba7865e676bf7ad504637e2d57c20563788b3f
EugeneHeen <eugeneheen_chen@126.com>
2017-09-12 09:25:38 +0800

    新增批量绑定角色权限关系

--- seedm-repository/src/main/java/com/github/seedm/repository/mapper/seed/IRolePermissionMapper.java
+++ seedm-repository/src/main/java/com/github/seedm/repository/mapper/seed/IRolePermissionMapper.java
@@ -2,6 +2,8 @@ package com.github.seedm.repository.mapper.seed;
 
 import com.github.seedm.repository.vo.seed.RolePermissionVo;
 
+import java.util.List;
+
 /**
  * 角色权限关系表映射操作
  */
@@ -13,4 +15,11 @@ public interface IRolePermissionMapper {
      * @return 成功绑定数据条数
      */
     public int insert(RolePermissionVo rolePermissionVo);
+
+    /**
+     * 批量新增角色权限关系绑定
+     * @param rolePermissions 角色权限关系绑定信息集合
+     * @return 成功新增角色权限关系绑定信息条数
+     */
+    public int insertMulti(List<RolePermissionVo> rolePermissions);
 }
