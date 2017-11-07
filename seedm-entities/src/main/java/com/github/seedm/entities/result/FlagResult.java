d5504fa9476a913c100be54b3a652d23f0c5f43e
EugeneHeen <eugeneheen_chen@126.com>
2017-11-07 16:41:35 +0800

    重命名标识结果类

--- seedm-entities/src/main/java/com/github/seedm/entities/result/FlagResult.java
+++ seedm-entities/src/main/java/com/github/seedm/entities/result/FlagResult.java
@@ -7,12 +7,70 @@ package com.github.seedm.entities.result;
 public class FlagResult {
 
     /**
-     * 标识符
+     * 未知
      */
-    public boolean flag;
+    public static final int UNKNOWN = -1;
 
     /**
-     * 结果
+     * 错误
      */
-    public Object result;
+    public static final int ERROR = 0;
+
+    /**
+     * 成功
+     */
+    public static final int SUCCESS = 1;
+
+    /**
+     * 结果标识符
+     */
+    public int flag = FlagResult.SUCCESS;
+
+    /**
+     * 结果附加消息
+     */
+    public String msg;
+
+    /**
+     * 结果数据
+     */
+    public Object data;
+
+    public FlagResult() {
+    }
+
+    public FlagResult(int flag, String msg) {
+        this.flag = flag;
+        this.msg = msg;
+    }
+
+    public FlagResult(int flag, String msg, Object data) {
+        this.flag = flag;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
