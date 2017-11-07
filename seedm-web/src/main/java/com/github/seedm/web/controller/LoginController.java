6b30114c9bc6bef6e5a37814dbcc9cf2812e777b
EugeneHeen <eugeneheen_chen@126.com>
2017-11-07 16:42:08 +0800

    优化登录实现

--- seedm-web/src/main/java/com/github/seedm/web/controller/LoginController.java
+++ seedm-web/src/main/java/com/github/seedm/web/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.github.seedm.web.controller;
 
+import com.github.seedm.entities.result.FlagResult;
 import com.github.toolkit.core.CodecKit;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,19 +30,27 @@ public class LoginController {
     private CodecKit codecKit;
 
     @RequestMapping(value = "/submit")
-    public String submit(HttpServletRequest request) {
+    @ResponseBody
+    public FlagResult submit(HttpServletRequest request) {
+        FlagResult result = new FlagResult();
         String mobile = request.getParameter("mobile");
         String password = request.getParameter("password");
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(codecKit.encodeBase64(mobile), codecKit.hex(password, CodecKit.ALGORITHMS_MD5));
         try {
             subject.login(usernamePasswordToken);
-            return "redirect:/";
+            result.setFlag(FlagResult.SUCCESS);
+            result.setMsg("登录成功");
+            return result;
         } catch (UnknownAccountException e) {//错误的账号
+            result.setFlag(FlagResult.ERROR);
+            result.setMsg("用户登录账号错误");
             logger.error("用户登录账号错误", e);
         } catch (IncorrectCredentialsException e) {//错误的凭证
+            result.setFlag(FlagResult.ERROR);
+            result.setMsg("用户登录密码错误");
             logger.error("用户登录密码错误", e);
         }
-        return "redirect:/main/login";
+        return result;
     }
 }
--- seedm-web/src/main/webapp/resources/js/login.js
+++ seedm-web/src/main/webapp/resources/js/login.js
@@ -1,15 +1,34 @@
 $(document).ready(function () {
     //登录
     $('#loginBut').click(function () {
-        var mobile = $('#mobile').val();
-        var password = $('#password').val();
+        var data = {
+            mobile: $('#mobile').val(),
+            password: $('#password').val()
+        };
 
-        if(mobile.length != 0 && password.length != 0) {
-            console.log('用户：' + mobile + ",使用密码：" + password + "登录了！");
-        } else {
-            console.log('No Account And Password!');
-            return false;
-        }
+        $.ajax({
+            type: "POST",
+            url: "/seedm/login/submit",
+            data: data,
+            dataType: 'json',
+            success: function(result){
+                console.log(result);
+                if(result && result.flag == 0) {
+                    console.log(result.msg);
+                } else if (result && result.flag == 1) {
+                    window.location.href = '/seedm/';
+                }
+            }
+        });
+
+        // if(mobile.length != 0 && password.length != 0) {
+        //     console.log('用户：' + mobile + ",使用密码：" + password + "登录了！");
+        // } else {
+        //     console.log('No Account And Password!');
+        //     return false;
+        // }
+
+        return false;
     });
 
     //忘记密码
