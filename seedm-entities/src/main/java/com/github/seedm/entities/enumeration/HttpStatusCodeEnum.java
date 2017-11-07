0767fa9a3b157b3a182e40db25d9b22d7669457c
EugeneHeen <eugeneheen_chen@126.com>
2017-11-07 16:41:01 +0800

    重命名Http状态枚举类

--- seedm-entities/src/main/java/com/github/seedm/entities/enumeration/HttpStatusCodeEnum.java
+++ seedm-entities/src/main/java/com/github/seedm/entities/enumeration/HttpStatusCodeEnum.java
@@ -4,14 +4,14 @@ package com.github.seedm.entities.enumeration;
  * Http状态码枚举
  * @author Eugene
  */
-public enum StatusCodeEnum {
+public enum HttpStatusCodeEnum {
 
     OK(200), BADREQUEST(400), NOTFOUND(404),
     INTERNALSERVERERROR(500), BADGATEWAY(502), SERVICEUNAVAILABLE(503), GATEWAYTIMEOUT(504), HTTPVERSIONNOTSUPPORTED(505);
 
     private int statusCode;
 
-    StatusCodeEnum(int statusCode) {
+    HttpStatusCodeEnum(int statusCode) {
         this.statusCode = statusCode;
     }
 
